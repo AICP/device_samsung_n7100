3e4533d6d695b9ea9867aa90c04021b6927bfcef
Zips <hcolmenares@gmail.com>
2014-06-26 09:33:08 -0400

    n7100: Welcome to AICP
    Change-Id: I8e3ffb297b842b98109c8e6e72bc7ea685df2394

--- full_n7100.mk
+++ full_n7100.mk
@@ -28,7 +28,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/n7100/n7100.mk)
 
 # Discard inherited values and use our own instead.
-PRODUCT_NAME := full_n7100
+PRODUCT_NAME := aicp_n7100
 PRODUCT_DEVICE := n7100
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
