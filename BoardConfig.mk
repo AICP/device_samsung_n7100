867c12720d362d0767067c5d3a18bc3fd7ff0fa6
tilaksidduram <tilaksidduram@gmail.com>
2016-09-20 15:52:19 +0530

    N7100: [Temp] Disable Selinux for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,5 +62,5 @@ RECOVERY_FSTAB_VERSION := 2
 SENSORS_NEED_SETRATE_ON_ENABLE := true
 
 # Selinux
-BOARD_SEPOLICY_DIRS += \
-    device/samsung/n7100/selinux
+#BOARD_SEPOLICY_DIRS += \
+#    device/samsung/n7100/selinux
