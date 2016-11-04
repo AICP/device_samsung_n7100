e44b8251c59a455296b650b32c757e3d50943b3a
Trafalgar Square <schneller.demian@gmail.com>
2016-11-04 13:43:18 +0100

    N7100: Set SELinux to permissive
    Change-Id: I836b0343280431df239c26886f62faf45539568e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,6 +42,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/n7100/bluetooth
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/smdk4412
 TARGET_KERNEL_CONFIG := cyanogenmod_n7100_defconfig
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
 
 # Power HAL
 TARGET_POWERHAL_VARIANT := samsung
